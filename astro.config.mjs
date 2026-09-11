ef78d6f45f5c4536f4ca778421b0a4e26b9816f0
Diego Rincon-Yanez <diegoprog@hotmail.com>
2026-09-11 14:28:41 +0200

    Deploying the site up adding contacts and projects, made by Diego Rincon-Yanez on 2026-09-11 at 14:28

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,33 +3,18 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import { SITE_URL } from './src/consts.ts';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://d1egoprog.github.io/website-develop/',
+	site: SITE_URL,
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
 			provider: fontProviders.local(),
 			name: 'Atkinson',
 			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
+			fallbacks: ['sans-serif']
 		},
 	],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,31 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const BASE_URL = "https://d1egoprog.github.io/website-develop/"
+export const SITE_URL = 'https://www.d1egoprog.co/';
+export const PERSON_NAME = 'Diego Rincon-Yanez';
+export const SITE_TITLE = PERSON_NAME;
+export const HOME_TITLE = `${PERSON_NAME} | Knowledge Graphs & Neuro-Symbolic AI`;
+export const SITE_DESCRIPTION =
+	'Diego Rincon-Yanez researches Knowledge Graphs and Neuro-Symbolic AI. Explore his publications, research projects, and academic experience.';
+export const BASE_URL = SITE_URL;
+
+export const PERSON = {
+	'@type': 'Person',
+	'@id': `${SITE_URL}#person`,
+	name: PERSON_NAME,
+	alternateName: 'd1egoprog',
+	url: SITE_URL,
+	image: new URL('img/avatars/casual.jpg', SITE_URL).href,
+	description: 'Computer science researcher focused on Knowledge Graphs and Neuro-Symbolic AI.',
+	knowsAbout: ['Knowledge Graphs', 'Neuro-Symbolic AI', 'Semantic Web'],
+	sameAs: [
+		'https://orcid.org/0000-0002-8982-1678',
+		'https://www.wikidata.org/wiki/Q112191114',
+		'https://scholar.google.com/citations?user=USbw71sAAAAJ',
+		'https://dblp.org/pid/282/8477.html',
+		'https://www.semanticscholar.org/author/Diego-Rincon-Yanez/2045763968',
+		'https://www.linkedin.com/in/d1egoprog/',
+		'https://x.com/d1egoprog',
+		'https://github.com/d1egoprog',
+	],
+};
